2661d96978e0988861cbeb144f2562c919f7dbaa
pandafam <suchint@gmail.com>
2023-10-20 12:15:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-pip install flask
-
-
-# In[ ]:
-
 
 from flask import Flask
 from flask import render_template,request
@@ -51,16 +43,6 @@ def index():
 
 if __name__ == "__main__":
     app.run()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
